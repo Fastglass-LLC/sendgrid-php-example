583e57fbc125bd30f1d76ac89074cf87855f3d2a
Brady Owens <info@fastglass.net>
2016-04-01 11:23:02 +0200

    Example script to send a simple email with a file attachment.

---
+++ sendgrid-php-example-send.php
@@ -0,0 +1,20 @@
+<?php
+require 'vendor/autoload.php';
+
+require_once 'config.php';
+
+$sendgrid = new SendGrid($sendgrid_api_key, ["turn_off_ssl_verification" => TRUE]);
+$email = new SendGrid\Email();
+$email->addTo($to)
+  ->setFrom($to)
+  ->setSubject('[sendgrid-php-example] Pachyderm named %yourname%')
+  ->setText('The elephant in the room is blue!')
+  ->setHtml('<strong>The %elephant% in the room is blue!</strong>')
+  ->addSubstitution('%yourname%', ['Mr. ElePHPant'])
+  ->addSubstitution('%elephant%', ['elephant'])
+  ->addHeader('X-Sent-Using', 'SendGrid-API')
+  ->addHeader('X-Transport', 'web')
+  ->addAttachment('./phplogo.gif', 'php.gif');
+
+$response = $sendgrid->send($email);
+var_dump($response);
